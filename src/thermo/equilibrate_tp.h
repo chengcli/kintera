1da442813e8e0c4d2f2a76d5e9c4dc6df55df74e
snowy/cli <chengcli@umich.edu>
2025-06-15 14:28:55 -0400

    revert

--- src/thermo/equilibrate_tp.h
+++ src/thermo/equilibrate_tp.h
@@ -70,7 +70,6 @@ int equilibrate_tp(T *gain, T *diag, T *xfrac, T temp, T pres, T const *stoich,
   }
 
   T *logsvp = (T *)malloc(nreaction * sizeof(T));
-  T *log_frac_sum = (T *)malloc(nreaction * sizeof(T));
 
   // weight matrix
   T *weight = (T *)malloc(nreaction * nspecies * sizeof(T));
@@ -108,8 +107,7 @@ int equilibrate_tp(T *gain, T *diag, T *xfrac, T temp, T pres, T const *stoich,
   int nactive = 0;
   T lambda = 0.;  // rate scale factor
   while (iter++ < *max_iter) {
-    /*printf("iter = %d, oversaturated = %d, lambda = %f\n", iter,
-    oversaturated, lambda);
+    /*printf("iter = %d\n ", iter);
     // print xfrac
     printf("xfrac = ");
     for (int i = 0; i < nspecies; i++) {
@@ -123,43 +121,39 @@ int equilibrate_tp(T *gain, T *diag, T *xfrac, T temp, T pres, T const *stoich,
       xg += xfrac[i];
     }
 
-    // reorder reaction set
-    // reset oversaturated if rates have been scaled
-    int oversaturated = 0;
-    int first = oversaturated;
+    // populate weight matrix, rhs vector and active set
+    int first = 0;
     int last = nreaction;
-
-    // inactive                  |<--------------->|
-    // undersaturated            |->               :
-    // oversaturated     |<----->|                 :
-    //               :...o       f                 :...l
-    //               :   |       |                 :   |
-    // | * * * * * * * | * * * * * * * * | * * * * * | x
-    // |---------------|-----------------|-----------|
-    // | OVERSATURATED | UNDERSATURATED  | INACTIVE  |
     while (first < last) {
       int j = reaction_set[first];
-      log_frac_sum[j] = 0.0;
+      T log_frac_sum = 0.0;
       T prod = 1.0;
 
       // active set condition variables
       for (int i = 0; i < nspecies; i++) {
         if (stoich[i * nreaction + j] < 0) {  // reactant
-          log_frac_sum[j] += (-stoich[i * nreaction + j]) * log(xfrac[i] / xg);
+          log_frac_sum += (-stoich[i * nreaction + j]) * log(xfrac[i] / xg);
         } else if (stoich[i * nreaction + j] > 0) {  // product
           prod *= xfrac[i];
         }
       }
 
-      if (log_frac_sum[j] > (logsvp[j] + logsvp_eps)) {  // oversaturated
-        int tmp = reaction_set[first];
-        reaction_set[first] = reaction_set[oversaturated];
-        reaction_set[oversaturated] = tmp;
-        oversaturated++;
-        if (oversaturated >= first) first++;
-      } else if (log_frac_sum[j] < (logsvp[j] - logsvp_eps) && prod > 0.) {
+      // active set, weight matrix and rhs vector
+      if ((log_frac_sum < (logsvp[j] - logsvp_eps) && prod > 0.) ||
+          (log_frac_sum > (logsvp[j] + logsvp_eps))) {
+        for (int i = 0; i < ngas; i++) {
+          weight[first * nspecies + i] = -stoich_sum[j] / xg;
+          if (stoich[i * nreaction + j] < 0) {
+            weight[first * nspecies + i] -=
+                stoich[i * nreaction + j] / xfrac[i];
+          }
+        }
+        for (int i = ngas; i < nspecies; i++) {
+          weight[first * nspecies + i] = 0.0;
+        }
+        rhs[first] = logsvp[j] - log_frac_sum;
         first++;
-      } else {  // inactive
+      } else {
         int tmp = reaction_set[first];
         reaction_set[first] = reaction_set[last - 1];
         reaction_set[last - 1] = tmp;
@@ -167,49 +161,13 @@ int equilibrate_tp(T *gain, T *diag, T *xfrac, T temp, T pres, T const *stoich,
       }
     }
 
-    /* print reaction set
-    printf("reaction_set = ");
-    for (int i = 0; i < oversaturated; i++) {
-      printf("%d ", reaction_set[i]);
-    }
-    printf("| ");
-    for (int i = oversaturated; i < first; i++) {
-      printf("%d ", reaction_set[i]);
-    }
-    printf("| ");
-    for (int i = first; i < nreaction; i++) {
-      printf("%d ", reaction_set[i]);
-    }
-    printf("\n");*/
-
-    // populate weight matrix and rhs vector
-    nactive = first;
-    for (int k = 0; k < nactive; k++) {
-      int j = reaction_set[k];
-      for (int i = 0; i < ngas; i++) {
-        weight[k * nspecies + i] = -stoich_sum[j] / xg;
-        if (stoich[i * nreaction + j] < 0) {
-          weight[k * nspecies + i] += (-stoich[i * nreaction + j]) / xfrac[i];
-        }
-      }
-
-      for (int i = ngas; i < nspecies; i++) {
-        weight[k * nspecies + i] = 0.0;
-      }
-
-      rhs[k] = logsvp[j] - log_frac_sum[j];
-    }
-
-    if ((first == nactive) && (lambda == 1.)) {
+    if (first == 0) {
       // all reactions are in equilibrium, no need to adjust saturation
-      bool check_equilibrium = true;
-      for (int k = 0; k < nactive; ++k) {
-        if (fabs(rhs[k]) > logsvp_eps) check_equilibrium = false;
-      }
-      if (check_equilibrium) break;
+      break;
     }
 
     // populate active stoichiometric and constraint matrix
+    nactive = first;
     for (int i = 0; i < nspecies; i++)
       for (int k = 0; k < nactive; k++) {
         int j = reaction_set[k];
@@ -240,7 +198,7 @@ int equilibrate_tp(T *gain, T *diag, T *xfrac, T temp, T pres, T const *stoich,
     // rate -> xfrac
     // copy xfrac to xfrac0
     memcpy(xfrac0, xfrac, nspecies * sizeof(T));
-    lambda = 1.;  // scale
+    T lambda = 1.;  // scale
     T xsum;
     while (true) {
       bool positive_vapor = true;
@@ -278,7 +236,6 @@ int equilibrate_tp(T *gain, T *diag, T *xfrac, T temp, T pres, T const *stoich,
   diag[0] = iter;
 
   free(logsvp);
-  free(log_frac_sum);
   free(rhs);
   free(weight);
   free(reaction_set);
