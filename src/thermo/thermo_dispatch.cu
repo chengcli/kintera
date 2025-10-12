a2377d72bd5f056c84277c2ffa7b032303a4a58c
mac/cli <chengcli@umich.edu>
2025-10-11 22:02:07 -0400

    wip

--- src/thermo/thermo_dispatch.cu
+++ src/thermo/thermo_dispatch.cu
@@ -5,8 +5,7 @@
 #include <c10/cuda/CUDAGuard.h>
 
 // kintera
-#include <kintera/utils/func1.hpp>
-#include <kintera/utils/func2.hpp>
+#include <kintera/utils/user_funcs.hpp>
 #include <kintera/loops.cuh>
 #include "equilibrate_tp.h"
 #include "equilibrate_uv.h"
