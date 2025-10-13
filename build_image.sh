173dcc952383192f909ed87159beac221a04f0e9
Cheng Li <chengcli@csrwks2024-0238.engin.umich.edu>
2025-10-13 14:30:28 -0400

    wip

--- build_image.sh
+++ build_image.sh
@@ -2,9 +2,9 @@
 
 # Enable BuildKit for better caching
 DOCKER_BUILDKIT=1 docker build \
-  --build-arg CUDA_VER=12.4.1 \
+  --build-arg CUDA_VER=12.9.1 \
   --build-arg UBUNTU_VER=22.04 \
   --build-arg USER_UID=$(id -u) \
   --build-arg USER_GID=$(id -g) \
-  -t cli/kintera:dev \
+  -t chengcli/kintera:dev \
   -f docker/Dockerfile .
